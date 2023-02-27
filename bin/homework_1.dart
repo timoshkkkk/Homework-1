5a80dfa03e426843e862ebe63174adbe7acf73e5
timoshkkkk <hahatimhaha@gmail.com>
2023-02-27 21:05:06 +0600

    first commit

--- bin/homework_1.dart
+++ bin/homework_1.dart
@@ -1,5 +1,22 @@
-import 'package:homework_1/homework_1.dart' as homework_1;
+void main(){
 
-void main(List<String> arguments) {
-  print('Hello world: ${homework_1.calculate()}!');
+double b = 4.20;
+int c = 3;
+
+print(b+c);
+
+ String text = 'Name';
+text = 'Flutter';
+print(text);
+
+String a = 'I want to learn Flutter';
+
+print(a);
+
+double myNumber=2.225;
+String myText;
+myText = '2.225';
+print(myText);
+
+print(myNumber.toString().runtimeType);
 }
